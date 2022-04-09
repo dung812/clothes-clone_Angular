e8b7f9b570e429688f4c2d1d251c6f416c83c6c7
dung812 <knguyn16@gmail.com>
2022-04-09 16:25:29 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CartComponent } from './cart/cart.component';
+import { BlogDetailComponent } from './blog/blog-detail/blog-detail.component';
+import { BlogListComponent } from './blog/blog-list/blog-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    // { path: '', redirectTo: '', pathMatch: 'full' },
+    { path: '', component: MainPageComponent }, // Mặc định sẽ route vào trang chủ
+    { path: 'mainpage', component: MainPageComponent },
+    { path: 'cart', component: CartComponent },
+    { path: 'checkout', component: CheckoutComponent },
+    { 
+      path: 'product', 
+      component: ProductListComponent,
+      children: [
+        { path: 'product-detail', component: ProductDetailComponent }
+      ]
+     },
+     { 
+      path: 'blog', 
+      component: BlogListComponent,
+      children: [
+        { path: 'blog-detail', component: BlogDetailComponent }
+      ]
+     },
+    { path: "**", component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { BlogListComponent } from './blog/blog-list/blog-list.component';
+import { BlogDetailComponent } from './blog/blog-detail/blog-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainPageComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    CartComponent,
+    CheckoutComponent,
+    BlogListComponent,
+    BlogDetailComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
