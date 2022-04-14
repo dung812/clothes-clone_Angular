7e27b8a1f4eaa6c6a28e2a598b4dfb84c09d8bb6
vhduy23 <vhduy23@gmail.com>
2022-04-14 13:30:55 +0700

    them anh, sua service

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -2,6 +2,7 @@ export interface PRODUCT {
     id: number;
     name: string;
     image: string;
+    alt: string;
     price: number;
     color: number;
     gender: string;
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,17 +9,16 @@ export class ProductService {
 
   getProducts() {
     const products: PRODUCT[]  = [
-      { id: 1, name: "side pants II", image: "../../../assets/images/product/for-him/him-1.jpeg", price: 449000, color: 2, gender: "him"},
-      { id: 2, name: "great life tee-2022", image: "../../../assets/images/product/for-him/him-2.jpeg", price: 179000, color: 2, gender: "him"},
-      { id: 3, name: "cuban basic shirt", image: "../../../assets/images/product/for-him/him-3.jpeg", price: 399000,color: 3, gender: "him"},
-      { id: 4, name: "sss wool polo shirt", image: "../../../assets/images/product/for-him/him-4.jpeg", price: 449000, color: 2, gender: "him"},
-      { id: 5, name: "cropped denim pants", image: "../../../assets/images/product/for-him/him-5.jpeg", price: 419000, color: 2, gender: "him"},
-      { id: 6, name: "Lady Knit" , image: "../../../assets/images/product/for-her/her-1a.jpeg", price: 379.000, color: 2, gender: "her"},
-      { id: 7, name: "Compo Bodysuit" , image: "../../../assets/images/product/for-her/her-2.jpeg", price: 319000, color: 2, gender: "her"},
-      { id: 8, name: "Flaming Jacket" , image: "../../../assets/images/product/for-her/her-3.jpeg", price: 899000, color: 3, gender: "her"},
-      { id: 9, name: "Semi-Set Shirt" , image: "../../../assets/images/product/for-her/her-4.jpeg", price: 349.000, color: 1, gender: "her"},      
-      { id: 10, name: "Spaghetti Strap Dress" , image: "../../../assets/images/product/for-her/her-5.jpeg", price: 349.000, color: 2, gender: "her"},      
-      { id: 11, name: "Dart Tuck Shirt" , image: "../../../assets/images/product/for-her/her-6a.jpeg", price: 419000, color: 2, gender: "her"},    
+      { id: 1, name: "side pants II", image: "../../../assets/images/product/for-him/him-1.jpeg", alt: "clothesforhim", price: 449000, color: 2, gender: "him"},
+      { id: 2, name: "great life tee-2022", image: "../../../assets/images/product/for-him/him-2.jpeg", alt: "clothesforhim", price: 179000, color: 2, gender: "him"},
+      { id: 3, name: "cuban basic shirt", image: "../../../assets/images/product/for-him/him-3.jpeg", alt: "clothesforhim", price: 399000,color: 3, gender: "him"},
+      { id: 4, name: "sss wool polo shirt", image: "../../../assets/images/product/for-him/him-4.jpeg", alt: "clothesforhim", price: 449000, color: 2, gender: "him"},
+      { id: 5, name: "cropped denim pants", image: "../../../assets/images/product/for-him/him-5.jpeg", alt: "clothesforhim", price: 419000, color: 2, gender: "him"},
+      { id: 6, name: "Lady Knit" , image: "../../../assets/images/product/for-her/her-1a.jpeg", alt: "clothesforher", price: 379.000, color: 2, gender: "her"},
+      { id: 7, name: "Compo Bodysuit" , image: "../../../assets/images/product/for-her/her-2.jpeg", alt: "clothesforhim", price: 319000, color: 2, gender: "her"},
+      { id: 8, name: "Flaming Jacket" , image: "../../../assets/images/product/for-her/her-3.jpeg", alt: "clothesforhim", price: 899000, color: 3, gender: "her"},
+      { id: 9, name: "Semi-Set Shirt" , image: "../../../assets/images/product/for-her/her-4.jpeg", alt: "clothesforhim", price: 349.000, color: 1, gender: "her"},      
+      { id: 10, name: "Spaghetti Strap Dress" , image: "../../../assets/images/product/for-her/her-5.jpeg", alt: "clothesforhim", price: 349.000, color: 2, gender: "her"},      
     ];
     return products;
   }
